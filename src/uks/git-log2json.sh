12cc9277b795df04f5a0ba96d2fa96d547e07fb6
Berko Szabolcs <szberko@gmail.com>
2016-03-15 19:43:36 +0100

    switch sh file from multiline to single line

--- src/uks/git-log2json.sh
+++ src/uks/git-log2json.sh
@@ -1,6 +1,2 @@
-git log \
-    --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'{  "commit": "%H",  "author": "%an <%ae>",  "date": "%ad",  "message": "%f"}' \
-    $@ | \
-    perl -pe 'BEGIN{print ""}; END{print "\n"}' | \
-    perl -pe 's/},]/}]/'> log.json
+git log --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'{  "commit": "%H",  "author": "%an <%ae>",  "date": "%ad",  "message": "%f"}' $@ | perl -pe 'BEGIN{print ""}; END{print "\n"}' | perl -pe 's/},]/}]/'> log.json
 
