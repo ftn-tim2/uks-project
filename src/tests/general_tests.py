329c8ee6327d26672176683c894d66cc5c45f52d
Berko Szabolcs <szberko@gmail.com>
2016-03-24 00:14:04 +0100

    started to implement first test case

---
+++ src/tests/general_tests.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+from selenium import webdriver
+
+class GeneralTest(unittest.TestCase):
+
+
+
+    def setUp(self):
+        chromedriver = "/usr/local/bin/chromedriver"
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        self.driver = webdriver.Chrome(chromedriver)
+
+    def test1(self):
+        driver = self.driver;
+        driver.get("http://localhost:8000")
+        login = driver.find_element_by_css_selector("a[href='/accounts/login/']")
+        login.click()
+        driver.find_element_by_id("id_username").send_keys("admin")
+        driver.find_element_by_id("id_password").send_keys("admin")
+        driver.find_element_by_css_selector(".btn-primary").click()
+
+
+    def tearDown(self):
+        self.driver.close();
+
+    if __name__ == "__main__":
+        unittest.main()
