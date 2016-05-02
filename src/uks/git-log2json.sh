ed82d5f9de9ee73790f86d05d2efb930fa18ac48
Kovacs Jozsef <thekrushka@gmail.com>
2016-05-02 07:36:12 -0700

    added sh header to .sh file

--- src/uks/git-log2json.sh
+++ src/uks/git-log2json.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 git log --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'{  "commit": "%H",  "author": "%an <%ae>",  "date": "%ad",  "message": "%f"}' $@ | perl -pe 'BEGIN{print ""}; END{print "\n"}' | perl -pe 's/},]/}]/'> log.json
 
