96749a6bacf358cf9ce884746bc153e138fafb7b
Kovacs Jozsef <thekrushka@gmail.com>
2016-05-28 06:14:20 -0700

    fix for #88

--- src/apps/views.py
+++ src/apps/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from uks.models import Project
+from django.db.models import Q
+from django.shortcuts import render
+
 from uks.models import Issue
+from uks.models import Project
 
 
 def home(request, template_name='index.html'):
     print("actual user " + request.user.username)
-    return render(request, template_name, {'projects': Project.objects.filter(owner = request.user.id),
-                                           'issues_assigned_to_me': Issue.objects.filter(assigned_to_id = request.user.id),
-                                           'issues_create_by_me': Issue.objects.filter(reporter_id = request.user.id)
-                                           })
+    return render(request, template_name,
+                  {'projects': Project.objects.filter(Q(owner=request.user.id) | Q(contributors=request.user.id)),
+                   'issues_assigned_to_me': Issue.objects.filter(
+                       assigned_to_id=request.user.id),
+                   'issues_create_by_me': Issue.objects.filter(reporter_id=request.user.id)
+                   })
