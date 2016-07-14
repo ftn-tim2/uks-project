495f73eda4553a04a5335a45d50eb042f8242bc0
Kovacs Jozsef <thekrushka@gmail.com>
2016-07-14 16:38:02 -0700

    fix : #92 may to many has to be handled with sticks and stones

--- src/apps/views.py
+++ src/apps/views.py
@@ -7,8 +7,16 @@ from uks.models import Project
 
 def home(request, template_name='index.html'):
     print("actual user " + request.user.username)
+    owned_projs = list(Project.objects.filter(Q(owner=request.user.id)))
+    contributed_projs_db = Project.objects.exclude(owner=request.user.id)
+
+    for proj in contributed_projs_db:
+        for con_user in proj.contributors.all():
+            if con_user.id == request.user.id:
+                owned_projs.append(proj)
+
     return render(request, template_name,
-                  {'projects': Project.objects.filter(Q(owner=request.user.id) | Q(contributors=request.user.id)),
+                  {'projects': owned_projs,
                    'issues_assigned_to_me': Issue.objects.filter(
                        assigned_to_id=request.user.id),
                    'issues_create_by_me': Issue.objects.filter(reporter_id=request.user.id)
